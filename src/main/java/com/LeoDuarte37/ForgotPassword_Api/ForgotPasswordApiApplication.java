3e504d4302f9d7f600e1768080ef067ab4271df2
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-07-13 15:16:24 -0300

    Init project

---
+++ src/main/java/com/LeoDuarte37/ForgotPassword_Api/ForgotPasswordApiApplication.java
@@ -0,0 +1,13 @@
+package com.LeoDuarte37.ForgotPassword_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgotPasswordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgotPasswordApiApplication.class, args);
+	}
+
+}
