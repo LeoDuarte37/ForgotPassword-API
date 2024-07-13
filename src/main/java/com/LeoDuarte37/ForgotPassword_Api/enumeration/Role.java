d03142b4a969fcb3a6e065f4ecf97d394599014c
Leo Duarte <leonardo.r.s.duarte@gmail.com>
2024-07-13 17:48:35 -0300

    Add getRoleByKey

--- src/main/java/com/LeoDuarte37/ForgotPassword_Api/enumeration/Role.java
+++ src/main/java/com/LeoDuarte37/ForgotPassword_Api/enumeration/Role.java
@@ -5,12 +5,24 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum Role {
     ADMIN (1),
     MANAGER (2),
-    COMMOM (3);
+    COMMON (3);
 
     private final int KEY;
 
     Role(int key) {
         this.KEY = key;
+    }
+
+    public static Role getRoleByKey(int key) {
+        if (key == 1) {
+            return ADMIN;
+        } else if (key == 2) {
+            return MANAGER;
+        } else if (key == 3) {
+            return COMMON;
+        } else {
+            return null;
+        }
     }
 
     @JsonValue
